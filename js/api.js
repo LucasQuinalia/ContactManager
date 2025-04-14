2096f1024ceb69712b91ea26dd2bdac1c0eab116
Lucas Quinália <lrgquinalia@gmail.com>
2025-04-14 20:45:45 -0300

    D delete implementation

--- js/api.js
+++ js/api.js
@@ -11,12 +11,31 @@ const api = {
         }
     },
 
+    async getContactById(id) {
+        try {
+            const response = await axios.get(`${URL_BASE}/contacts/${id}`)
+            return await response.data
+        } catch {
+            alert('Error getting contact by id')
+            throw error
+        }
+    },
+
     async createContact(contact) {
         try {
             const response = await axios.post(`${URL_BASE}/contacts`, { ...contact })
             return await response.data
         } catch {
             alert("Error creating new contact")
+            throw error
+        }
+    },
+
+    async deleteContact(id) {
+        try {
+            const response = await axios.delete(`${URL_BASE}/contacts/${id}`)
+        } catch {
+            alert('Error deleting contact')
             throw error
         }
     }
